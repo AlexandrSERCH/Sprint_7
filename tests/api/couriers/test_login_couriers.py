a46e6b376506718256742ad761f2186f50a6e1c7
Александр <alexandr.serch.qa@gmail.com>
2026-02-19 22:52:47 +0200

    Реализовать тесты на авторизацию пользователя

---
+++ tests/api/couriers/test_login_couriers.py
@@ -0,0 +1,58 @@
+import allure
+import pytest
+import requests
+
+from helpers.create_couriers import register_new_courier_and_return_login_password
+from setting import BASE_URL
+
+
+@allure.epic("Курьер")
+@allure.feature("Авторизация курьера")
+class TestLoginCouriers:
+
+    PATH = "/api/v1/courier/login"
+
+    @allure.title("Успешая авторизация курьера")
+    def test_login_courier_returns_200_and_id(self):
+
+        login_and_password = register_new_courier_and_return_login_password()
+
+        payload = {
+            "login": login_and_password[0],
+            "password": login_and_password[1]
+        }
+
+        response = requests.post(f"{BASE_URL}{self.PATH}", data=payload)
+        body = response.json()
+
+        assert response.status_code == 200
+        assert body.get("id") is not None
+
+    @allure.title("Ошибка валидации при отсутствии поля 'login'")
+    def test_login_courier_without_login_returns_400(self):
+
+        payload = {
+            "password" : "123"
+        }
+
+        response = requests.post(f"{BASE_URL}{self.PATH}", data=payload)
+        body = response.json()
+
+        assert response.status_code == 400
+        assert body["message"] == "Недостаточно данных для входа"
+
+    @allure.title("Ошибка валидации при отсутствии поля 'password'")
+    def test_login_courier_without_password_returns_400(self):
+
+        payload = {
+            "login" : "user"
+        }
+
+        try:
+            response = requests.post(f"{BASE_URL}{self.PATH}", data=payload, timeout=5)
+            body = response.json()
+        except:
+            pytest.fail("Превышено время ожидания: 5 секунд")
+
+        assert response.status_code == 400
+        assert body["message"] == "Недостаточно данных для входа"

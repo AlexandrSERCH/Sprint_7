4359c43ed12752cf9bbfbab6b4cbf87cd9dbc0e4
Александр <alexandr.serch.qa@gmail.com>
2026-02-21 21:50:29 +0200

    Реализовать тесты удаления курьера

--- tests/api/couriers/test_delete_courier.py
+++ tests/api/couriers/test_delete_courier.py
@@ -11,13 +11,30 @@ class TestDeleteCourier:
     PATH = "/api/v1/courier/"
 
     @allure.title("Успешное удаление курьера")
-    def test_delete_courier_returns_200(self):
-        courier_id = 123
+    def test_delete_courier_returns_200(self, auth_courier):
 
-        response = requests.delete(f"{BASE_URL}{self.PATH}/{courier_id}")
-        print(response.status_code)
-        print(response.json())
+        response = requests.delete(f"{BASE_URL}{self.PATH}{auth_courier}")
+        body = response.json()
 
-        #
-        #  ПРОДОЛЖИТЬ РЕАЛИЗОВЫВАТЬ ТЕСТ
-        #
+        assert response.status_code == 200
+        assert body == {'ok': True}
+
+    @allure.title("Ошибка валидации при удалении курьера без id")
+    def test_delete_courier_without_id_returns_400(self):
+
+        response = requests.delete(f"{BASE_URL}{self.PATH}")
+        body = response.json()
+
+        assert response.status_code == 400
+        assert body["message"] == "Недостаточно данных для удаления курьера"
+
+    @allure.title("Ошибка валидации при удалении курьера c невалидным id")
+    def test_delete_courier_with_invalid_id_retutns_404(self):
+
+        invalid_id = 123987
+
+        response = requests.delete(f"{BASE_URL}{self.PATH}{invalid_id}")
+        body = response.json()
+
+        assert response.status_code == 404
+        assert body["message"] == "Курьера с таким id нет."
